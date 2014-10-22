fceed5503535771320bd4fd3933cb5c9244b7280
eral.r.l <eral.r.l+github.com@gmail.com>
2014-10-23 02:47:13 +0900

    WIP

---
+++ Assets/App/2dTest/Collider2dOneTimeTesting.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider2dOneTimeTesting : MonoBehaviour {
+	public Collider2D	m_A;
+	public Collider2D	m_B;
+	public Condition	m_Pass = Condition.Hit;
+
+	public enum Condition {
+		Hit,
+		NoHit,
+		ThrowSystemArgumentNullException,
+		ThrowUnknownColliderException,
+		ThrowSystemNotImplementedException,
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		try {
+			if (ColliderIsHitExtention.IsHit(m_A, m_B)) {
+				Pass(Condition.Hit);
+			} else {
+				Pass(Condition.NoHit);
+			}
+		} catch (ColliderIsHitExtention.UnknownColliderException) {
+			Pass(Condition.ThrowUnknownColliderException);
+		} catch (System.ArgumentException) {
+			Pass(Condition.ThrowSystemArgumentNullException);
+		} catch (System.NotImplementedException) {
+			Pass(Condition.ThrowSystemNotImplementedException);
+		} catch {
+			IntegrationTest.Fail(gameObject);
+		}
+	}
+
+	private void Pass(Condition cond) {
+		if (cond == m_Pass) {
+			IntegrationTest.Pass(gameObject);
+		} else {
+			IntegrationTest.Fail(gameObject);
+		}
+	}
+}

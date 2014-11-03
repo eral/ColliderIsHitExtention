e83a09f03598d629050f1385aea0397f201316ad
eral.r.l <eral.r.l+github.com@gmail.com>
2014-11-04 02:53:58 +0900

    ColliderIsHitExtention:Separate 3d and 2d
    3Dと2Dをファイル分離

---
+++ Assets/App/Product/ColliderIsHitExtention.Common.cs
@@ -0,0 +1,17 @@
+﻿// (C) 2014 ERAL
+// Distributed under the Boost Software License, Version 1.0.
+// (See copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class ColliderIsHitExtention : MonoBehaviour {
+
+	public class UnknownColliderException: System.NotImplementedException {
+		public UnknownColliderException(): base() {}
+		public UnknownColliderException(string message): base(message) {}
+		public UnknownColliderException(string message, System.Exception innerException): base(message, innerException) {}
+	}
+}
